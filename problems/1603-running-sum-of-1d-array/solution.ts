70c1de16d47f4505842c3a3d8821b91eea5a52dd
Ася Кульвановская <nastena.kulvanovskaya.02@mail.ru>
2025-08-05 08:39:58 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55.8 MB (85.14%)

--- problems/1603-running-sum-of-1d-array/solution.ts
+++ problems/1603-running-sum-of-1d-array/solution.ts
@@ -1,11 +1,4 @@
 function runningSum(nums: number[]): number[] {
-  let result = [nums[0]];
-  let prevSum = nums[0]
-
-  for (let i = 1; i < nums.length; i++) {
-    result.push(prevSum + nums[i])
-    prevSum += nums[i]
-  }
-
-  return result
+  let result = 0
+  return nums.map(item => result += item)
 };
