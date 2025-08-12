2b645ae79e066326794f6d7646a0d7d1ed961059
Ася Кульвановская <nastena.kulvanovskaya.02@mail.ru>
2025-08-12 13:43:35 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 56 MB (19.88%)

--- problems/1791-richest-customer-wealth/solution.ts
+++ problems/1791-richest-customer-wealth/solution.ts
@@ -1,15 +1,3 @@
 function maximumWealth(accounts: number[][]): number {
-  let clientsWealth: number[] = [];
-
-  for (let i = 0; i < accounts.length; i++) {
-    let currentClientWealth = accounts[i][0];
-
-    for (let j = 1; j < accounts[i].length; j++) {
-      currentClientWealth += accounts[i][j]
-    }
-
-    clientsWealth.push(currentClientWealth);
-  }
-
-  return Math.max(...clientsWealth)
-};
+  return Math.max(...accounts.map(customer => customer.reduce((clientWealth, current) => clientWealth + current, 0)));
+}
