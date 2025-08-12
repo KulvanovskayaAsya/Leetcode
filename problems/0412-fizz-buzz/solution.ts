52729df4dbffd7ebc38ab34b44d0815831c5dfe2
Ася Кульвановская <nastena.kulvanovskaya.02@mail.ru>
2025-08-12 13:56:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 58.9 MB (46.32%)

---
+++ problems/0412-fizz-buzz/solution.ts
@@ -0,0 +1,19 @@
+function fizzBuzz(n: number): string[] {
+  let answer = [];
+
+  for (let i = 1; i <= n; i++) {
+    let result = '';
+
+    if (i % 3 === 0) {
+      result += 'Fizz'
+    }
+
+    if (i % 5 === 0) {
+      result += 'Buzz'
+    }
+    
+    answer.push(result || i.toString())
+  }
+
+  return answer;
+};
