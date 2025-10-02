f81af0c22db45db954dc6fc691cd4e7a20c24738
Ася Кульвановская <nastena.kulvanovskaya.02@mail.ru>
2025-10-02 13:33:55 +0000

    [LeetCode Sync] Runtime - 7 ms (23.35%), Memory - 58.6 MB (29.94%)

--- problems/1168-duplicate-zeros/solution.ts
+++ problems/1168-duplicate-zeros/solution.ts
@@ -1,12 +1,38 @@
 /**
  Do not return anything, modify arr in-place instead.
  */
-function duplicateZeros(arr: number[]): void {    
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 0) {
-            arr.splice(i, 0, arr[i]);
-            arr.pop()
-            i++;
+/**
+ Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr: number[]): void {
+  let zeroesCount = 0;
+  let to = arr.length - 1;
+
+  for (let i = 0; i <= to - zeroesCount; i++) {
+    if (arr[i] === 0) {
+        if (i == to - zeroesCount) {
+            arr[to] = 0;
+            to--;
+            break;
         }
+
+        zeroesCount++
+    };
+  }
+
+  let from = to - zeroesCount;
+
+  while (from >= 0) {
+    arr[to] = arr[from];
+
+    if (arr[from] === 0) {
+        arr[to - 1] = 0;
+        to--;
     }
-};
+
+    to--;
+    from--;
+  }
+
+  console.log(arr);
+}
